3e22b623b226bb3df7c1b50d109b5db32e70f4d8
Vitchyr Pong <vitchyr@gmail.com>
2014-12-22 21:59:49 -0500

    add file reader

--- src/main/scala/market/coindeskmarket.scala
+++ src/main/scala/market/coindeskmarket.scala
@@ -1,17 +1,29 @@
 import defs._
 import scala.util.Random.nextDouble
+import scala.io.Source
 
 package market { 
   object CoinDeskMarket extends VirtualMarket {
     val dataFileName: String = "data/coindesk-bpi-USD-close_data-2013-12-21" +
       "_2014-12-21.csv"
+
+    // Given a csv two two columns (first is date, second is price)
+    def getCoinDeskDataFrom(fname: String): Array[Double] = {
+      val src = Source.fromFile(fname)
+      val iter = src.getLines().drop(1) // drop the header
+      val data = iter map (_.split(",")(1).toDouble)
+      src.close()
+      data.toArray
+    }
+
     private var dataIdx: Int = 0 // index into data where we currently are.
+    private val data: Array[Double] = getCoinDeskDataFrom(dataFileName)
 
     def iterator = new Iterator[Double] {
       def hasNext = true
       def next = {
-        100
         dataIdx += 1
+        100
       }
     }
 
