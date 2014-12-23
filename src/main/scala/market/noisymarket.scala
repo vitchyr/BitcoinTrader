22b224318a7eaf8b4a8f1f2cebab4a66ee49257e
Vitchyr Pong <vitchyr@gmail.com>
2014-12-22 21:47:07 -0500

    remove even more unused members

--- src/main/scala/market/noisymarket.scala
+++ src/main/scala/market/noisymarket.scala
@@ -3,8 +3,6 @@ import util.Random.nextDouble
 package market {
   // A market whose price goes up and down according to a sinusoid + noise
   class NoisyMarket(fm: FakeMarket) extends FakeMarket {
-    private var t: Double = 0
-    private val dt = 0.01
     private val An = 3 // amplitude of noise
 
     def iterator = new Iterator[Double] {
