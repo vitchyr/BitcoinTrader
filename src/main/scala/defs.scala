cc808f50ed3bb8234602c51ef3e79164629ccc71
Vitchyr Pong <vitchyr@gmail.com>
2014-12-22 21:27:29 -0500

    add rounding logic

--- src/main/scala/defs.scala
+++ src/main/scala/defs.scala
@@ -1,10 +1,27 @@
 // Define definitions that everyone will use
 
 package defs {
-  // Holds how many bitcoins changes for how much cash in [currency]
-  class Transaction(val dBitcoin: Double, val dCash: Double,
-      val currency: String)
+  /* Holds how many bitcoins were sold/bought for how much cash in [currency]
+   * at a given time.
+   *     dB = change in bitcoin
+   *     dC = change in Cash
+   */
+  class Transaction(dB: Double, dC: Double,
+      val time: Long, val currency: String) {
+    val dBitcoins = Transaction.roundBtc(dB)
+    val dCash = Transaction.roundCash(dC)
+  }
 
-  // Holds statistics about bitcoins at some point in time
-  class BitcoinInfo(val price: Double, val time: Double, val currency: String)
+  object Transaction {
+    val CashPrecision: Int = 2 // 2 decimal place. Rounds to the cent
+    val BtcPrecision: Int = 4 // 4 decimal place.
+
+    def round(d: Double, precision: Int): Double =
+      BigDecimal(d).setScale(precision, BigDecimal.RoundingMode.FLOOR)
+        .toDouble
+
+    def roundBtc(x: Double): Double = round(x, BtcPrecision)
+
+    def roundCash(x: Double): Double = round(x, CashPrecision)
+  }
 }
