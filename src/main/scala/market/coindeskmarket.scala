98d56ca041d7be9b1ebb31ac1e1d3fd4f24fa524
Vitchyr Pong <vitchyr@gmail.com>
2014-12-24 14:34:29 -0500

    Finished using real data

--- src/main/scala/market/coindeskmarket.scala
+++ src/main/scala/market/coindeskmarket.scala
@@ -1,31 +1,35 @@
 import defs._
 import scala.util.Random.nextDouble
 import scala.io.Source
+import java.io.{FileReader, FileNotFoundException, IOException}
 
 package market { 
   object CoinDeskMarket extends VirtualMarket {
-    val dataFileName: String = "data/coindesk-bpi-USD-close_data-2013-12-21" +
-      "_2014-12-21.csv"
+    val dataFileName: String = "data/"+
+      "coindesk-bpi-USD-close_data-2013-12-21_2014-12-21.csv"
 
     // Given a csv two two columns (first is date, second is price)
     def getCoinDeskDataFrom(fname: String): Array[Double] = {
       val src = Source.fromFile(fname)
       val iter = src.getLines().drop(1) // drop the header
       val data = iter map (_.split(",")(1).toDouble)
+      val out = data.toArray
       src.close()
-      data.toArray
+      out
     }
 
-    private var dataIdx: Int = 0 // index into data where we currently are.
+    private var i: Int = 0 // index into data where we currently are.
     private val data: Array[Double] = getCoinDeskDataFrom(dataFileName)
 
     def iterator = new Iterator[Double] {
-      def hasNext = true
+      def hasNext = i < data.length
       def next = {
-        dataIdx += 1
-        100
+        i += 1
+        data(i - 1)
       }
     }
+
+    def resetState(): Unit = i = 0
 
     override def toString = "CoinDesk Market"
   }
