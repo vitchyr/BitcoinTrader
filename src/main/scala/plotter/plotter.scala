1de741697e226f1e4007a926926141d2ae5fb310
Vitchyr Pong <vitchyr@gmail.com>
2015-01-13 19:48:46 -0500

    add hack to have nice plot endpoints

--- src/main/scala/plotter/plotter.scala
+++ src/main/scala/plotter/plotter.scala
@@ -11,6 +11,12 @@ package plotter {
       val (mTs, mPrices) = (for (h <- t.m.history) yield (h.time.toDouble, h.price))
         .unzip
 
+      // hack to make the plots nice
+      val dT = (mTs.tail.head - mTs.head) * mTs.length / 100
+      val endpointTs = List(mTs.head - dT, mTs.last + dT)
+      val lowPlim = mPrices.min - (mPrices.max - mPrices.min) / 10
+      val endpointPs = List(lowPlim, lowPlim)
+
       val tData = for (h <- t.history) yield (h.time, h.dBitcoins, h.dCash)
       val (buyData, sellData) = tData partition { case(_, dB, _) => dB > 0 }
       val (buyTs, buyPrices) = (buyData map
@@ -18,18 +24,13 @@ package plotter {
       val (sellTs, sellPrices) = (sellData map
         { case (t, dB, dC) => (t.toDouble, -dC/ dB) }).unzip
 
-      //println("Buy time, sell time:")
-      //(buyTs zip sellTs) foreach println
-
-      //println(s"$t data:")
-      //tData foreach println
-
       val f = Figure()
       val p = f.subplot(0)
       p += plot(mTs, mPrices, '-')
       p += plot(buyTs, buyPrices, '+')
       p += plot(sellTs, sellPrices, '.')
-      p.xlabel = "Time (+ = buy, - = sell)"
+      p += plot(endpointTs, endpointPs, '.')
+      p.xlabel = "Time (+ = buy, . = sell)"
       p.ylabel = "Price ($ / BTC)"
       p.title = s"Transactions of ${t.name} at ${t.m} vs. Time" + 
         f"\n(returns = ${t.returns}%3.2f%%)"
